e13d2a35a31f77d21a5ef162a94a6f196efcb666
shimisunet <xxnanochan@gmail.com>
2018-01-13 11:20:49 +0900

    mentionにもユーザ名を表示させるようにした

--- twitter.rb
+++ twitter.rb
@@ -20,7 +20,7 @@ end
 
 # display mentions
 client.mentions_timeline.each do |tweet|
-  puts tweet.user.screen_name.+":"+tweet.text
+  puts tweet.user.name + "[ID:" + tweet.user.screen_name + "]:" + tweet.text
 end
 
 # tweet
