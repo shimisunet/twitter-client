a118d62b9d9d3ac6f7f313fd6f8bf4e2116c4ac1
shimisunet <xxnanochan@gmail.com>
2018-01-13 12:43:19 +0900

    ユーザ名とツイート内容の間を改行して見やすくした

--- twitter.rb
+++ twitter.rb
@@ -14,12 +14,14 @@ end
 
 # display timeline
 client.home_timeline.each do |tweet|
-  puts tweet.user.name + "[ID:" + tweet.user.screen_name + "]:" + tweet.text
+  puts tweet.user.name + "[ID:" + tweet.user.screen_name + "]"
+  puts tweet.text
 end
 
 # display mentions
 client.mentions_timeline.each do |tweet|
-  puts tweet.user.name + "[ID:" + tweet.user.screen_name + "]:" + tweet.text
+  puts tweet.user.name + "[ID:" + tweet.user.screen_name + "]"
+  puts tweet.text
 end
 
 # display list
